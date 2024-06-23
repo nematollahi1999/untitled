cb8b9901a9a1a547097567576b3cbba9c0667918
nematollahi.1999 <nematollahi.1999@gmail.com>
2024-06-23 22:11:05 +0330

    consts file created.

---
+++ utils/consts.js
@@ -0,0 +1,221 @@
+// data/tshirts.js
+
+export const tshirts = [
+    { body: 'fit', neck: 'loose', sleeve: 'normal', src: 'images/01' },
+    { body: 'fit', neck: 'loose', sleeve: 'long', src: 'images/02' },
+    { body: 'fit', neck: 'loose', sleeve: 'less', src: 'images/03' },
+    { body: 'fit', neck: 'normal', sleeve: 'normal', src: 'images/04' },
+    { body: 'fit', neck: 'normal', sleeve: 'long', src: 'images/05' },
+    { body: 'fit', neck: 'normal', sleeve: 'less', src: 'images/06' },
+    { body: 'fit', neck: 'mock', sleeve: 'normal', src: 'images/07' },
+    { body: 'fit', neck: 'mock', sleeve: 'long', src: 'images/08' },
+    { body: 'fit', neck: 'mock', sleeve: 'less', src: 'images/09' },
+    { body: 'oversized', neck: 'loose', sleeve: 'normal', src: 'images/10' },
+    { body: 'oversized', neck: 'loose', sleeve: 'long', src: 'images/11' },
+    { body: 'oversized', neck: 'loose', sleeve: 'less', src: 'images/12' },
+    { body: 'oversized', neck: 'normal', sleeve: 'normal', src: 'images/13' },
+    { body: 'oversized', neck: 'normal', sleeve: 'long', src: 'images/14' },
+    { body: 'oversized', neck: 'normal', sleeve: 'less', src: 'images/15' },
+    { body: 'oversized', neck: 'mock', sleeve: 'normal', src: 'images/16' },
+    { body: 'oversized', neck: 'mock', sleeve: 'long', src: 'images/17' },
+    { body: 'oversized', neck: 'mock', sleeve: 'less', src: 'images/18' },
+  ];
+
+  
+  export const neckItems = [
+    {
+      title: "نوع یقه",
+      header: "انتخاب نوع یقه",
+      detail:
+        "نوع یقه برحسب سلیقه کاربر روی راحتی و انعطاف حرکتی گردن تاثیر مستقیم دارد.",
+      samples: "/",
+      options: [
+        {
+          title: "یقه نرمال",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "یقه سه سانتی",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "یقه ماک",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+      ],
+    },
+    {
+      title: "فیت یقه",
+      header: "تعیین اندازه یقه",
+      detail:
+        "نوع یقه برحسب سلیقه کاربر روی راحتی و انعطاف حرکتی گردن تاثیر مستقیم دارد.",
+      samples: "/",
+      options: [
+        {
+          title: "لوس فیت",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "نرمال فیت",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "تایت فیت",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+      ],
+    },
+    {
+      title: "دوخت یقه",
+      header: "تعیین دوخت یقه",
+      detail:
+        "نوع یقه برحسب سلیقه کاربر روی راحتی و انعطاف حرکتی گردن تاثیر مستقیم دارد.",
+      samples: "/",
+      options: [
+        {
+          title: "تک نخه",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "دو نخه",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "دوخت مخفی",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+      ],
+    },
+  ];
+
+
+  export const bodyItems = [
+    {
+      title: "نوع تنه",
+      header: "انتخاب نوع بدنه",
+      detail:
+        "نوع بدنه برحسب سلیقه کاربر روی راحتی و انعطاف حرکتی گردن تاثیر مستقیم دارد.",
+      samples: "/",
+      options: [
+        {
+          title: "نرمال",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "اورسایز",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        ],
+    },
+    {
+      title: "دوخت تنه",
+      header: "تعیین دوخت یقه",
+      detail:
+        "نوع تنه برحسب سلیقه کاربر روی راحتی و انعطاف حرکتی گردن تاثیر مستقیم دارد.",
+      samples: "/",
+      options: [
+        {
+          title: "تک نخه",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "دو نخه",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "دوخت مخفی",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+      ],
+    },
+  ];
+
+  export const sleeveItems = [
+    {
+      title: "نوع آستین",
+      header: "انتخاب نوع آستین",
+      detail:
+        "نوع آستین برحسب سلیقه کاربر روی راحتی و انعطاف حرکتی گردن تاثیر مستقیم دارد.",
+      samples: "/",
+      options: [
+        {
+          title: "یقه نرمال",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "یقه سه سانتی",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "یقه ماک",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+      ],
+    },
+    {
+      title: "فیت آستین",
+      header: "تعیین اندازه آستین",
+      detail:
+        "نوع آستین برحسب سلیقه کاربر روی راحتی و انعطاف حرکتی گردن تاثیر مستقیم دارد.",
+      samples: "/",
+      options: [
+        {
+          title: "لوس فیت",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "نرمال فیت",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "تایت فیت",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+      ],
+    },
+    {
+      title: "دوخت آستین",
+      header: "تعیین دوخت آستین",
+      detail:
+        "نوع آستین برحسب سلیقه کاربر روی راحتی و انعطاف حرکتی گردن تاثیر مستقیم دارد.",
+      samples: "/",
+      options: [
+        {
+          title: "تک نخه",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "دو نخه",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+        {
+          title: "دوخت مخفی",
+          subtitle: "یقه عادی یک سانتی",
+          img: "/images/neck.jpg",
+        },
+      ],
+    },
+  ];
+
